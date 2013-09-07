d177d2dd58b4b60ec005dc85b67cafc767434284
peihsinsu <simonsu.mail@gmail.com>
2013-09-08 02:01:18 +0800

    update getJobList bug, modify snip function

--- test2.js
+++ test2.js
@@ -5,8 +5,8 @@ initor.genPrj('express', ['-e', '-s'], '/tmp/test2', function(){
   // add module
   initor.addNpmModule('/tmp/test2', 'nodeutil');
   // add snip
-  initor.getSnip('/tmp/test2', 'mysql', 'mysql-config', function(){
-    console.log('add snip done...');
+  initor.getSnip('/tmp/test2', 'mysql', 'mysql-config', function(d){
+    console.log('add snip done..., msg:' + JSON.stringify(d) );
   });
 });
 
