648268763deb77dcc52c3c65e954482001c3bab4
clonn <clonncd@gmail.com>
2013-09-07 23:47:03 +0800

    add angular for init app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,26 @@
   var init;
 
   init = function($) {
-    return console.log('hello world');
+    var EditCtrl, ListCtrl, app;
+    console.log('hello world');
+    app = angular.module('app', []);
+    app.config(function($routeProvider) {
+      return $routeProvider.when('/', {
+        controller: ListCtrl,
+        templateUrl: '/template/index.html'
+      }).when('/home', {
+        controller: ListCtrl,
+        templateUrl: '/html/listitem.html'
+      }).otherwise({
+        redirectTo: '/'
+      });
+    });
+    ListCtrl = function($scope) {
+      return console.log('run list ctrl');
+    };
+    return EditCtrl = function($scope) {
+      return console.log('edit ctrl');
+    };
   };
 
   init(jQuery);
--- src/public/javascripts/app.coffee
+++ src/public/javascripts/app.coffee
@@ -1,6 +1,24 @@
 init = ($) ->
   console.log 'hello world'
 
+  app = angular.module('app', [])
+
+  app.config ($routeProvider) ->
+    $routeProvider.when '/',
+      controller: ListCtrl
+      templateUrl: '/template/index.html'
+    .when '/home',
+      controller: ListCtrl
+      templateUrl: '/html/listitem.html'
+    .otherwise
+      redirectTo:'/'
+
+  ListCtrl = ($scope) ->
+    console.log 'run list ctrl'
+
+  EditCtrl = ($scope) ->
+    console.log 'edit ctrl'
+
 # initial
 init(jQuery)
 
