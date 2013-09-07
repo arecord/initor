08bd658adbfcdaf516a0828eb87c7a0c5d016b9d
clonn <clonncd@gmail.com>
2013-09-08 01:12:44 +0800

    update create new preojct

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@
   var init;
 
   init = function($) {
-    var EditCtrl, ListCtrl, app;
+    var EditCtrl, ListCtrl, app, createDetailCtrl;
     console.log('hello world');
     app = angular.module('app', []);
     app.config(function($routeProvider) {
@@ -12,6 +12,9 @@
       }).when('/create', {
         controller: ListCtrl,
         templateUrl: '/template/create.html'
+      }).when('/create/:chooseType', {
+        controller: createDetailCtrl,
+        templateUrl: '/template/createDetail.html'
       }).when('/projectList', {
         controller: ListCtrl,
         templateUrl: '/template/projectList.html'
@@ -19,8 +22,32 @@
         redirectTo: '/'
       });
     });
+    createDetailCtrl = function($scope, $routeParams) {
+      var createType;
+      $scope.createData = {
+        'express': {
+          id: 'express',
+          name: 'express basic type',
+          desc: 'expresswhos works base on node.js and very light way.'
+        },
+        'database': {
+          id: 'database',
+          name: 'database type',
+          desc: 'database nd you can choose you own database mix with node.js.'
+        }
+      };
+      createType = $routeParams.chooseType;
+      $scope.blur = function() {
+        if ($scope.projectName.length > 0) {
+          return $("#projectName").addClass("success").find(".help-block").show();
+        } else {
+          return $("#projectName").removeClass("success").find(".help-block").hide();
+        }
+      };
+      return $scope.createModel = $scope.createData[createType];
+    };
     ListCtrl = function($scope) {
-      return console.log('run list ctrl');
+      return console.log('run list');
     };
     return EditCtrl = function($scope) {
       return console.log('edit ctrl');
--- src/public/javascripts/app.coffee
+++ src/public/javascripts/app.coffee
@@ -10,15 +10,37 @@ init = ($) ->
     .when '/create',
       controller: ListCtrl
       templateUrl: '/template/create.html'
+    .when '/create/:chooseType',
+      controller: createDetailCtrl
+      templateUrl: '/template/createDetail.html'
     .when '/projectList',
       controller: ListCtrl
       templateUrl: '/template/projectList.html'
     .otherwise
       redirectTo:'/'
 
-  ListCtrl = ($scope) ->
-    console.log 'run list ctrl'
+  createDetailCtrl = ($scope, $routeParams) ->
+    $scope.createData =
+      'express':
+        id: 'express'
+        name: 'express basic type'
+        desc: 'expresswhos works base on node.js and very light way.'
+      'database':
+        id: 'database'
+        name: 'database type'
+        desc: 'database nd you can choose you own database mix with node.js.'
 
+    createType = $routeParams.chooseType
+    $scope.blur = () ->
+      if ($scope.projectName.length > 0)
+        $("#projectName").addClass("success").find(".help-block").show()
+      else
+        $("#projectName").removeClass("success").find(".help-block").hide()
+
+    $scope.createModel = $scope.createData[createType]
+
+  ListCtrl = ($scope) ->
+    console.log 'run list'
   EditCtrl = ($scope) ->
     console.log 'edit ctrl'
 
