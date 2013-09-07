a80d6e34ba3cd3ca56c080b2f7dc0fdcc6ddd8f0
peihsinsu <simonsu.mail@gmail.com>
2013-09-08 01:52:30 +0800

    update snip function

--- test2.js
+++ test2.js
@@ -5,6 +5,8 @@ initor.genPrj('express', ['-e', '-s'], '/tmp/test2', function(){
   // add module
   initor.addNpmModule('/tmp/test2', 'nodeutil');
   // add snip
-  initor.getSnip('/tmp/test2', 'mysql', 'mysql-config');
+  initor.getSnip('/tmp/test2', 'mysql', 'mysql-config', function(){
+    console.log('add snip done...');
+  });
 });
 
