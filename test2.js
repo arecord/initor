c176238578c7363a9a39d1abaa25a27ea766cc3a
peihsinsu <simonsu.mail@gmail.com>
2013-09-08 01:38:33 +0800

    add snip related operation

--- test2.js
+++ test2.js
@@ -1,6 +1,10 @@
 var initor = require('./lib/initor');
 
+// generate project
 initor.genPrj('express', ['-e', '-s'], '/tmp/test2', function(){
+  // add module
   initor.addNpmModule('/tmp/test2', 'nodeutil');
+  // add snip
+  initor.getSnip('/tmp/test2', 'mysql', 'mysql-config');
 });
 
