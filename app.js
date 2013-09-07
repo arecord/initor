7ce56a6b173d73cc71cefe8519b8242e6e5cea51
clonn <clonncd@gmail.com>
2013-09-07 20:45:57 +0800

    update all

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
+/*
+Module dependencies.
+*/
 
-/**
- * Module dependencies.
- */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+(function() {
+  var app, express, http, partials, path, routes, user;
 
-var app = express();
+  express = require("express");
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+  routes = require("./routes");
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+  user = require("./routes/user");
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+  http = require("http");
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  path = require("path");
+
+  partials = require('express-partials');
+
+  app = express();
+
+  app.set("port", process.env.PORT || 3000);
+
+  app.set("views", __dirname + "/views");
+
+  app.set("view engine", "ejs");
+
+  app.use(partials());
+
+  app.use(express.logger("dev"));
+
+  app.use(express.bodyParser());
+
+  app.use(express.methodOverride());
+
+  app.use(app.router);
+
+  app.use(express["static"](path.join(__dirname, "public")));
+
+  if ("development" === app.get("env")) {
+    app.use(express.errorHandler());
+  }
+
+  app.get("/", routes.index);
+
+  app.get("/users", user.list);
+
+  http.createServer(app).listen(app.get("port"), function() {
+    return console.log("Express server listening on port " + app.get("port"));
+  });
+
+}).call(this);
--- src/app.coffee
+++ src/app.coffee
@@ -7,12 +7,14 @@ routes = require("./routes")
 user = require("./routes/user")
 http = require("http")
 path = require("path")
+partials = require('express-partials')
 app = express()
 
 # all environments
 app.set "port", process.env.PORT or 3000
 app.set "views", __dirname + "/views"
 app.set "view engine", "ejs"
+app.use partials()
 #app.use express.favicon()
 app.use express.logger("dev")
 app.use express.bodyParser()
